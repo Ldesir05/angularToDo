1cea7fe717479fd6dbc4ebe106a0ee62aaf5753e
Ldesir05 <ldesir5@mail.valenciacollege.edu>
2017-03-08 15:12:56 -0500

    small changes to list and toDo file

--- models/toDo.js
+++ models/toDo.js
@@ -6,25 +6,25 @@ var Schema = mongoose.Schema;
 
 // Create a ToDo with the Schema class
 var ToDoSchema = new Schema({
-  // name of to-do item
-  item: {
-    type: String
-  },
-  // completed - boolean
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  // date the entry was created
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  // date the item is due - default is one day later
+    // name of to-do item
+    item: {
+        type: String
+    },
+    // completed - boolean
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    // date the entry was created
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    // date the item is due - default is one day later
     due: {
-    type: Date,
-    default: new Date(+new Date() + 24*60*60*1000)
-  }
+        type: Date,
+        default: new Date(+new Date() + 24 * 60 * 60 * 1000)
+    }
 });
 
 // Make a ToDo model with the ToDoSchema
